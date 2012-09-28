ff311af595c133786fda2a0e3fd6dafa800eaf91
BA CodeMonkey <bmbrands@gmail.com>
2012-09-28 07:44:36 +0200

    clean version of bootstrap

---
+++ version.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Theme version info
+ *
+ * @package    theme
+ * @subpackage bootstrap
+ * @copyright  2012 Bas Brands, www.basbrands.nl
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2012092700; // The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2011120500; // Requires this Moodle version
+$plugin->maturity = MATURITY_BETA;             // this version's maturity level
+$plugin->release = '1.0 (Build: 20122092700)';
+$plugin->component = 'theme_bootstrap'; // Full name of the plugin (used for diagnostics)
